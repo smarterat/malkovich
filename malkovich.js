7f026d9af26097790afcd8eb06c1e7048226204b
Ryan <ryan@unknown7cc3a1a1b1b7.gateway.2wire.net>
2013-09-05 10:59:36 -0400

    Initial commit

---
+++ malkovich.js
@@ -0,0 +1,52 @@
+function malkovich(MALKOVICH, Malkovich) {
+    Malkovich &&( malkovich. malkovich =[,
+    MALKOVICH, MALKOVICH = Malkovich = malkovich.
+    $Malkovich =-( MALKOVICH ==( malkovich.
+    Malkovich = Malkovich ))], malkovich.
+    malkovich [Malkovich++] = malkovich. malkovich, malkovich.
+    MALKOVICH = ++Malkovich, malkovich (--Malkovich), malkovich.
+    $Malkovich = ++Malkovich, malkovich. MALKOVICH++ );
+    Malkovich = malkovich. Malkovich [malkovich.
+    $Malkovich++ ]; malkovich. Malkovich = MALKOVICH? Malkovich?
+    '\012'== Malkovich? malkovich (++ MALKOVICH, malkovich.
+    malkovich [++ malkovich. MALKOVICH ]=
+    MALKOVICH - MALKOVICH ): Malkovich
+    ==' '|'\015'== Malkovich ||
+    (Malkovich   )== "m" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "a" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "l" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "k" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "o" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "v" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "i" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "c" &  malkovich. Malkovich [malkovich.
+    $Malkovich++ ]== "h"&&++malkovich. malkovich [malkovich.
+    MALKOVICH]? malkovich (MALKOVICH)
+    :[ "Error on line "+MALKOVICH+": expected 'malkovich'",
+       malkovich. MALKOVICH = MALKOVICH ++- MALKOVICH ]:
+    malkovich. malkovich :( MALKOVICH = malkovich.
+    Malkovich[malkovich.MALKOVICH], Malkovich? (Malkovich =
+
+    --Malkovich? --Malkovich? --Malkovich? --Malkovich? --Malkovich?
+    --Malkovich? --Malkovich? --Malkovich? --Malkovich?
+    malkovich. MALKOVICH++ &&
+    --Malkovich :'&#'+MALKOVICH+';': malkovich.
+    Malkovich [malkovich. Malkovich [-- malkovich. MALKOVICH ]&&
+    (malkovich. $Malkovich += MALKOVICH), --malkovich.
+    MALKOVICH ]: malkovich. Malkovich [malkovich.
+    Malkovich [MALKOVICH] = malkovich. Malkovich
+    [-- malkovich. MALKOVICH ],-- malkovich. MALKOVICH ]:
+    malkovich. Malkovich [malkovich. Malkovich [malkovich.
+    $Malkovich++ ]] [MALKOVICH]: MALKOVICH == malkovich.
+    Malkovich [-- malkovich. MALKOVICH ]:
+    MALKOVICH*malkovich. Malkovich [-- malkovich.
+    MALKOVICH ]: malkovich. Malkovich [-- malkovich.
+    MALKOVICH ]- MALKOVICH: malkovich. Malkovich [-- malkovich.
+    MALKOVICH ]+ MALKOVICH: malkovich.
+    MALKOVICH ++ && "malkovich", malkovich.
+    Malkovich [malkovich. MALKOVICH ]= Malkovich, malkovich
+    ()): MALKOVICH );
+
+    return malkovich.
+    Malkovich
+}
